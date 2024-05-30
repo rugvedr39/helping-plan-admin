d48859097b5c7043fa6626b8b5f22967fd5d6bc9
Rugved-Technify <rugvedr39@gmail.com>
2024-05-30 10:08:00 +0530

    link updated
    link updated

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -6,8 +6,8 @@ import { Observable } from "rxjs";
   providedIn: "root",
 })
 export class UserService {
-  private apiUrl = "http://localhost:3000/api/";
-  // private apiUrl = "https://helpingplan.online/api/";
+  // private apiUrl = "http://localhost:3000/api/";
+  private apiUrl = "https://helpingplan.online/api/";
 
   constructor(private http: HttpClient) {}
 
